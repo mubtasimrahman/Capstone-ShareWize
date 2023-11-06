48085044c18986fe0cd841ddbb22428fc98e8acf
Mubtasim Rahman <rahmam91@mcmaster.ca>
2023-11-05 23:16:13 -0500

    Initial Commit

--- Project Source Code/ShareWize/client/src/App.tsx
+++ Project Source Code/ShareWize/client/src/App.tsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { HomePage } from './pages/HomePage'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import  HomePage  from './pages/HomePage';
 
 function App() {
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <HomePage></HomePage>
-    </>
-  )
+    <Router>
+      {/* Your app components */}
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
+
--- Project Source Code/ShareWize/client/src/pages/HomePage.tsx
+++ Project Source Code/ShareWize/client/src/pages/HomePage.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const HomePage = () => {
+function Homepage() {
   return (
-    <div>ShareWize</div>
-  )
+    <div className="homepage">
+      <header>
+        <h1>Expense Manager</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/expenses">Expenses</Link>
+          </li>
+          <li>
+            <Link to="/groups">Groups</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <section>
+          <h2>Welcome to Expense Manager</h2>
+          <p>Manage your expenses with ease and transparency. Join or create expense groups, track your spending, and settle up with friends.</p>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Log In</Link>
+        </section>
+      </main>
+    </div>
+  );
 }
+
+export default Homepage;
+

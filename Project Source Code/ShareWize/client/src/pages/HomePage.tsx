e7ca94bd66a25e8c6e494e65dca39e53e90fa7ae
Mubtasim Rahman <rahmam91@mcmaster.ca>
2023-11-13 22:59:01 -0500

    Third Commit

--- Project Source Code/ShareWize/client/src/pages/HomePage.tsx
+++ Project Source Code/ShareWize/client/src/pages/HomePage.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Introduction from "../components/Introduction/Introduction";
 import { Navbar } from "../components/Navbar/Navbar";
+import "./HomePage.css";
 
 function Homepage() {
   return (
     <>
-      
-        {/* <header>
+      {/* <header>
           <h1>Expense Manager</h1>
         </header> */}
-        {/* <nav>
+      {/* <nav>
           <ul>
             <li>
               <Link to="/dashboard">Dashboard</Link>
@@ -25,19 +25,18 @@ function Homepage() {
             </li>
           </ul>
         </nav> */}
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col">
-              <Navbar></Navbar>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-            <Introduction></Introduction>
-            
-            </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <Navbar></Navbar>
           </div>
         </div>
+        <div className="row intro">
+          <div className="col">
+            <Introduction></Introduction>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

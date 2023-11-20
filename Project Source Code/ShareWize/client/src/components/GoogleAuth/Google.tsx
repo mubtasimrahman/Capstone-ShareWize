400acf2971e4abdcf0458f3fb3a99956bb0652f4
Mubtasim Rahman <rahmam91@mcmaster.ca>
2023-11-19 19:23:53 -0500

    Google Auth integration partial more

--- Project Source Code/ShareWize/client/src/components/GoogleAuth/Google.tsx
+++ Project Source Code/ShareWize/client/src/components/GoogleAuth/Google.tsx
@@ -6,14 +6,15 @@ function Google() {
   const [user, setUser] = useState({});
   return (
     <GoogleLogin
+      useOneTap
       onSuccess={(credentialResponse) => {
         console.log(credentialResponse);
         if (credentialResponse.credential) {
           // Check if credentialResponse.credential is defined
           const userObject = jwtDecode(credentialResponse.credential);
           console.log(userObject);
-          setUser(userObject)
-          console.log(user)
+          setUser(userObject);
+          console.log(user);
         } else {
           // Handle the case where credentialResponse.credential is undefined
           console.error("Credential is undefined");
